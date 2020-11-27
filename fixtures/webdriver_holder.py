f603d76c7f431f86663ac2f5624c1a594df53a81
Blazej Wielk <bwielk@gmail.com>
2020-11-27 19:59:30 +0000

    replaced the webdriver manager- works now

--- fixtures/webdriver_holder.py
+++ fixtures/webdriver_holder.py
@@ -1,13 +1,10 @@
 import pytest
-from webdrivermanager import ChromeDriverManager
 from selenium import webdriver
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def gen_web_driver():
-    chrome_driver = ChromeDriverManager()
-    path_to_driver = chrome_driver.download_and_install()[0]
-    driver = webdriver.Chrome(executable_path=path_to_driver)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.fanduel.com/")
     driver.maximize_window()
     return driver
