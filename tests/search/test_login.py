75b2ca14a0230277e4da85927e709e7753e7950d
Blazej Wielk <bwielk@gmail.com>
2020-12-01 18:26:30 +0000

    replaced the webdriver manager- works now

--- tests/search/test_login.py
+++ tests/search/test_login.py
@@ -5,6 +5,6 @@ class TestSearch:
     def test_failed_login_message_is_correct(self, gen_web_driver):
         home_page.click_login_page(gen_web_driver)
         home_page.select_fantasy_login_option(gen_web_driver)
-        login_page.fill_email_field("hello@hello.com")
-        login_page.fill_password_field("password")
+        login_page.fill_email_field(gen_web_driver, "hello@hello.com")
+        login_page.fill_password_field(gen_web_driver, "password")
         login_page.check_failed_login_content(gen_web_driver)
